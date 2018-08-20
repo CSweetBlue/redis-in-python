296403dfb2a4524539951e486681139e6d33d4f4
CSweetBlue <csweet@utexas.edu>
2018-08-19 23:24:25 -0500

    Added methods for handling Redis data transfer protocol.

--- redisDatabase.py
+++ redisDatabase.py
@@ -12,9 +12,51 @@ class Disconnect(Exception): pass
 Error = namedtuple('Error', ('message',))
 
 class ProtocolHandler(object):
+    def __init__(self):
+        self.handlers = {
+            '+': self.handle_simple_string,
+            '-': self.handle_error,
+            ':': self.handle_integer,
+            '$': self.handle_string,
+            '*': self.handle_array,
+            '%': self.handle_dict,
+        }
+
     def handle_request(self, socket_file):
-        pass
+        first_byte = socket_file.read(1)
+        if not first_byte:
+            raise Disconnect()
+
+        try:
+            return self.handlers[first_byte](socket_file)
+        except KeyError:
+            raise CommandError('Bad Request.')
     
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+    
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+
+        if length == -1:
+            return None
+        
+        length +=2
+
+        return socket_file.read(length)[:-2]
+    
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file) for _ in range(num_items * 2)]
+
+        return dict(zip(elements[::2], elements[1::2]))
+
     def write_response(self, socket_file, data):
         pass
 
@@ -47,4 +89,3 @@ class Server(object):
 
     def run(self):
         self._server.serve_forever()
-        
