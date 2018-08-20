2896c14a8f9590aff95186044583ab2a39d5f286
CSweetBlue <csweet@utexas.edu>
2018-08-20 09:41:04 -0500

    Wrote the _write handler in ProtocolHandler.

--- redisDatabase.py
+++ redisDatabase.py
@@ -53,12 +53,45 @@ class ProtocolHandler(object):
     
     def handle_array(self, socket_file):
         num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
         elements = [self.handle_request(socket_file) for _ in range(num_items * 2)]
 
         return dict(zip(elements[::2], elements[1::2]))
 
     def write_response(self, socket_file, data):
-        pass
+        buf = BytesIO()
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buf.write('$%s\r\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buf.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buf.wirte('-%s\r\n' % Error.message)
+        elif isinstance(data, (list, tuple)):
+            buf.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError('unrecognized type: %s' % type(data))
+
 
 class Server(object):
     def __init__(self, host='127.0.0.1', port= 31337, max_clients=64):
